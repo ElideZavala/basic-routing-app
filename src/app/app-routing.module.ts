9a9a9125100a4327c6c28301f7cc7e47bd123692
Elide <zavalavinagreelide@gmail.com>
2023-08-05 21:12:48 -0600

    feat: Se crean rutas de Home y about
    incluimos igualmente los estilos indicados para cada una de las dos paginas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'landing',
+    pathMatch: 'full',
+  },
+  {
+    path: 'landing',
+    component: LandingComponent,
+  },
+  {
+    path: '*',
+    redirectTo: 'landing',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
